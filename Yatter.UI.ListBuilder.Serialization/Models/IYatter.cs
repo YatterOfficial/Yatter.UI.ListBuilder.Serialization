5d0be208502942effeb02e5aeba1366eef0cc1a5
Count Anthony Harrison <anthony@harrisonofthenorth.com>
2021-09-24 15:06:02 +0100

    Modified Yatter syntax

--- Yatter.UI.ListBuilder.Serialization/Models/IYatter.cs
+++ Yatter.UI.ListBuilder.Serialization/Models/IYatter.cs
@@ -5,8 +5,8 @@ namespace Yatter.UI.ListBuilder.Serialization.Models
 {
     public interface IYatter
     {
-        string DataType { get; }
-        Dictionary<string, string> YatterSpace { get; set; }
+        string DataType { get; set; }
+        Dictionary<string, string> YatterSpaces { get; set; }
         Dictionary<string, string> ServiceDictionary { get; set; }
         List<Object> Items { get; set; }
     }
--- Yatter.UI.ListBuilder.Serialization/Models/Yatter.cs
+++ Yatter.UI.ListBuilder.Serialization/Models/Yatter.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Yatter.UI.ListBuilder.Serialization.Models
 {
     public class Yatter : IYatter
     {
-        public string DataType => "Yatter";
-        public Dictionary<string,string> YatterSpace { get; set; }
-        public Dictionary<string,string> ServiceDictionary { get; set; }
+        [JsonIgnore]
+        private string _dataType;
+
+        public string DataType
+        {
+            get
+            {
+                return _dataType;
+            }
+            set
+            {
+                _dataType = value;
+            }
+        }
+
+        public Dictionary<string, string> YatterSpaces { get; set; }
+        public Dictionary<string, string> ServiceDictionary { get; set; }
         public List<Object> Items { get; set; }
+
+        public Yatter()
+        {
+            _dataType = GetType().ToString();
+        }
     }
 }
