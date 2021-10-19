835f3375de704e9d102a28ee3d35d3bc9493a708
Count Anthony Harrison <anthony@harrisonofthenorth.com>
2021-10-19 21:54:22 +0100

    YatterPath extended to accept a PageLoaderKey

--- Yatter.UI.ListBuilder.Serialization/Models/YatterPath.cs
+++ Yatter.UI.ListBuilder.Serialization/Models/YatterPath.cs
@@ -4,7 +4,9 @@ namespace Yatter.UI.ListBuilder.Serialization.Models
     public class YatterPath
     {
         public string Title { get; set; }
+        public string Username { get; set; }
         public string Path { get; set; }
+        public string PageLoaderKey { get; set; }
         public string Mnemonic { get; set; }
     }
 }
