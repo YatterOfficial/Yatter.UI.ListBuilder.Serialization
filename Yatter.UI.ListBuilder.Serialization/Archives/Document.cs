0abd31e60a7167a52d478f1a8c393e231073c064
Count Anthony Harrison <anthony@harrisonofthenorth.com>
2021-10-28 00:17:15 +0100

    Interim

---
+++ Yatter.UI.ListBuilder.Serialization/Archives/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Yatter.UI.ListBuilder.Serialization.Archives
+{
+    public class Document
+    {
+        public string DataType { get; set; }
+
+        public string Path { get; set; }
+        public string Base64Content { get; set; }
+
+        public Document()
+        {
+            DataType = GetType().ToString();
+        }
+    }
+}
+
---
+++ Yatter.UI.ListBuilder.Serialization/Archives/Magazine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yatter.UI.ListBuilder.Serialization.Archives
+{
+    public class Magazine
+    {
+        public Magazine()
+        {
+            DataType = GetType().ToString();
+            PathRoot = default(string);
+        }
+
+        public string DataType { get; set; }
+        public Dictionary<string, string> YatterSpaces = new Dictionary<string, string>();
+
+        public string PathRoot { get; set; }
+        public List<Document> Documents = new List<Document>();
+    }
+}
+
